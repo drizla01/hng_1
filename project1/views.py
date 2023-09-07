11038f0abdd1a76a322fae65a1b2d53622605129
Awal Umar <awalumar277@gmail.com>
2023-09-07 07:51:58 +0100

    reorganie

--- project1/views.py
+++ project1/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+import datetime
+import json
 
 def get_info(request):
     #the get query from url
@@ -9,13 +11,20 @@ def get_info(request):
     if not slack_name or not track:
         return JsonResponse({"error":"Both slack name and track must be provided"}, status=400)
     
+    github_repo = "https://github.com/drizla01/hng_1"
+    github_file = "f'{github.com/drizla01}/blob/master/api_endpoint/views.py"
+    cur_day = datetime.datetime.utcnow().strftime('%A')
+    utctime = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
+
+
+
     response_data = { 
-        "slack_name": "awal_umar",
-        "current_day": "Thursday",
-        "utc_time": "2023-09-21T15:04:05Z",
-        "track": "backend",
-        "github_file_url": "https://github.com/drizla01/hng_1/blob/main/file_name.ext",
-        "github_repo_url": "https://github.com/drizla01/hng_1",
+        "slack_name": slack_name,
+        "current_day": cur_day,
+        "utc_time": utctime,
+        "track": track,
+        "github_file_url": github_file,
+        "github_repo_url": github_repo,
         "status_code": 200
   }
     return JsonResponse(response_data)
