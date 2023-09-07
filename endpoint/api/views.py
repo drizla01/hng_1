76f7aa252ba7c849b0e074679bf3f94c9de17680
Awal Umar <awalumar277@gmail.com>
2023-09-07 18:02:47 +0100

    almost done

--- endpoint/api/views.py
+++ endpoint/api/views.py
@@ -1,18 +1,19 @@
 from django.http import JsonResponse
 import datetime
-import json
+from django.http import HttpResponseBadRequest
 
 def get_info(request):
     #the get query from url
-    slack_name = request.GET.get("awal_umar")
-    track = request.GET.get("backend")
+    slack_name = request.GET.get('awal_umar')
+    track = request.GET.get('backend')
 
     #to confirm that the query are provided
     if not slack_name or not track:
-        return JsonResponse({"error":"Both slack name and track must be provided"}, status=400)
+        error_message ="Both slack name and track must be provided"
+        return HttpResponseBadRequest(error_message)
     
     github_repo = "https://github.com/drizla01/hng_1"
-    github_file = "f'{github.com/drizla01}/blob/master/api/views.py"
+    github_file = "github.com/drizla01/hng_1/blob/master/api/views.py"
     cur_day = datetime.datetime.utcnow().strftime('%A')
     utctime = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
 
